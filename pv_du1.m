cf1b0aa2b94c094ab3d7f286eedad6da1ecef5da
Martin Gráf <40501219+Neathac@users.noreply.github.com>
2023-03-11 00:08:26 +0100

    Add files via upload

---
+++ pv_du1.m
@@ -0,0 +1,19 @@
+%% uloha 1
+% Nacitajte obrazok 'Lighthouse_bggr.png'. Je to informacia o hodnotach 
+% intenzity v Bayerovej mriezke. Bayerova mriezka je typu BGGR. 
+% Vytvorte farebny obraz jednoduchym priemerovanim hodnot v 3x3 okoli. 
+% Porovnajte vysledok s vysledkom funkcie demosaic v MATLABe alebo
+% demosaicing() (pripadne cvtColor()) v OpenCV
+
+
+%% uloha 2
+% Vytvorte funkciu na najdenie farebnej palety obrazu pomocou delenia podla
+% medianu. Predpokladajte ze obraz je v priestore RGB. Vstupnym parametrom
+% bude pocet farieb. Ak farieb v obraze bude menej ako vstup, vrati sa
+% povodna mnozina farieb obrazu.
+
+
+%% uloha 3
+% Zo vztahu pre minimalizaciu strednej kvadratickej chybu kvantovania 
+% odvodte, ze plati podmienka najblizsieho suseda a podmienka centroidu.
+
